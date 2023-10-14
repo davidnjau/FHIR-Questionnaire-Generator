a54d7d18913c2514baed249be01aef45fe6d37a3
davidnjau <davidnjau21@gmail.com>
2023-10-14 18:02:32 +0300

    first commit

---
+++ src/test/java/com/questionnaire/fhir/FhirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questionnaire.fhir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
