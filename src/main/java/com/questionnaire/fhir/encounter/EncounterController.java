e7bc0e04e72b6faffda02b0044f24b518d9fc590
davidnjau <davidnjau21@gmail.com>
2023-10-14 18:07:55 +0300

    Added encounter

---
+++ src/main/java/com/questionnaire/fhir/encounter/EncounterController.java
@@ -0,0 +1,11 @@
+package com.questionnaire.fhir.encounter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/encounter/api/v1/")
+public class EncounterController {
+}
